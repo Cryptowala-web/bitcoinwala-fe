16253769fccde071f933a0460e1269f8d367c65f
ushasri2645 <ugudikandula@everest.engineering>
2025-06-16 10:17:23 +0530

    Update ROutes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-/* eslint-disable no-unused-vars */
 import HomePage from "./pages/home";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence, motion, useScroll, useTransform } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import AdminManagement from "./pages/AdminDashboard";
+import Register from "./pages/Register";
+import NotFoundPage from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UnauthorizedPage from "./pages/UnAuthorisedPage";
 
 function App() {
   const location = useLocation();
@@ -30,26 +41,98 @@ function App() {
   return (
     <>
       <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route 
-          path="/" 
-          element={
-            <motion.div
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              variants={pageVariants}
-              transition={pageTransition}
-            >
-              <HomePage />
-            </motion.div>
-          }
-        />
-      </Routes>
-    </AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <HomePage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <Login />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/forgot-password"
+            element={
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <ForgotPassword />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <motion.div
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={pageVariants}
+                  transition={pageTransition}
+                >
+                  <AdminManagement />
+                </motion.div>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <Register />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/unauthorized"
+            element={
+              <motion.div
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <UnauthorizedPage />
+              </motion.div>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
 
 export default App;
-
