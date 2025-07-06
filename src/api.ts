c2c670bd22092b4d031137cd43ad888c5b58b7b7
Cryptowala-web <info@cryptowala.ai>
2025-07-06 19:09:06 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
 // export const API = 'http://localhost:5040'
-export const API = "https://bitcoin-wala-backend-production.up.railway.app" 
+export const API = "https://bitcoinwala-be.onrender.com" 
