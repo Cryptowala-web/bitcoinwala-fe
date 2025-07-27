65a1a0b6473086b92a0f8ff4096e3517d02ab747
ushasri2645 <ugudikandula@everest.engineering>
2025-07-27 21:33:58 +0530

    update backend path

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,3 @@
 // export const API = 'http://localhost:5040'
-export const API = "https://bitcoinwala-be.onrender.com" 
+// export const API = "https://bitcoinwala-be.onrender.com" 
+export const API = 'https://bitcoinwala-be-production-1d45.up.railway.app'
