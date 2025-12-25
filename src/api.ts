e9d3a4f93ca7bdc392602e487aafb0bf07f76676
USHASRI1112 <96167956+USHASRI1112@users.noreply.github.com>
2025-12-26 02:05:34 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
 // export const API = 'http://localhost:5040'
-export const API = "https://bitcoinwala-be.onrender.com" 
+// export const API = "https://bitcoinwala-be.onrender.com" 
+export const API = "https://bitcoinwala-be-h5s8.onrender.com"
 // export const API = 'https://bitcoinwala-be-production-1d45.up.railway.app'
